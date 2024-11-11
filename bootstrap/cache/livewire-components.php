3ce953090c5eb7a45c3a66eab7bdfdb70f1c637a
siihasann <hasannajib3@gmail.com>
2024-11-11 13:10:22 +0700

    fixdeploy4

---
+++ bootstrap/cache/livewire-components.php
@@ -0,0 +1,8 @@
+<?php return array(
+  'departement.index' => 'App\\Http\\Livewire\\Departement\\Index',
+  'employee.index' => 'App\\Http\\Livewire\\Employee\\Index',
+  'eselon.index' => 'App\\Http\\Livewire\\Eselon\\Index',
+  'group.index' => 'App\\Http\\Livewire\\Group\\Index',
+  'layout.sidebar-component' => 'App\\Http\\Livewire\\Layout\\SidebarComponent',
+  'unit.index' => 'App\\Http\\Livewire\\Unit\\Index',
+);
