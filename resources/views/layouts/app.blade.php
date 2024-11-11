4e8a218a7ace9a58dfd958eff594c2183a9027c6
siihasann <hasannajib3@gmail.com>
2024-11-11 12:55:24 +0700

    fixdeploy2

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -1,45 +1,53 @@
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
-    <head>
-        <meta charset="utf-8">
-        <meta name="viewport" content="width=device-width, initial-scale=1">
-        <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <title>{{ config('app.name', 'Laravel') }}</title>
+<head>
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <meta name="csrf-token" content="{{ csrf_token() }}">
 
-        <!-- Fonts -->
-        <link rel="preconnect" href="https://fonts.bunny.net">
-        <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
+    <title>{{ config('app.name', 'Laravel') }}</title>
 
-        <!-- Scripts -->
-        @vite(['resources/css/app.css', 'resources/js/app.js'])
-        @livewireStyles
-    </head>
-    <body class="font-sans antialiased">
-        <div class="flex min-h-screen bg-gray-100 dark:bg-gray-900">
-            <!-- Sidebar Livewire Component -->
-            @livewire('layout.sidebar-component')
+    @vite(['resources/css/app.css', 'resources/js/app.js'])
 
-            <!-- Main Content -->
-            <div class="flex-1">
-                @include('layouts.navigation')
+    <!-- Fonts -->
+    <link rel="preconnect" href="https://fonts.bunny.net">
+    <link href="https://fonts.bunny.net/css?family=figtree:400,500,600&display=swap" rel="stylesheet" />
 
-                <!-- Page Heading -->
-                @if (isset($header))
-                    <header class="bg-white dark:bg-gray-800 shadow">
-                        <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
-                            {{ $header }}
-                        </div>
-                    </header>
-                @endif
+    <!-- Scripts -->
 
-                <!-- Page Content -->
-                <main>
-                    {{ $slot }}
-                </main>
-            </div>
+    <!-- Scripts -->
+    <script src="https://kit.fontawesome.com/b30d6eb604.js" crossorigin="anonymous"></script>
+
+    @livewireStyles
+</head>
+
+<body class="font-sans antialiased">
+    <div class="flex min-h-screen bg-gray-100 dark:bg-gray-900">
+        <!-- Sidebar Livewire Component -->
+        @livewire('layout.sidebar-component')
+
+        <!-- Main Content -->
+        <div class="flex-1">
+            @include('layouts.navigation')
+
+            <!-- Page Heading -->
+            @if (isset($header))
+            <header class="bg-white dark:bg-gray-800 shadow">
+                <div class="max-w-7xl mx-auto py-6 px-4 sm:px-6 lg:px-8">
+                    {{ $header }}
+                </div>
+            </header>
+            @endif
+
+            <!-- Page Content -->
+            <main>
+                {{ $slot }}
+            </main>
         </div>
-        
-        @livewireScripts
-    </body>
+    </div>
+
+    @livewireScripts
+</body>
+
 </html>
